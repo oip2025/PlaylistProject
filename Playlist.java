7c06f0364c992c88db53c1a6f0497722a4102fd2
Olivia Ip <oip25@students.polytechnic.org>
2025-01-27 11:55:50 -0800

    Added some methods into Playlist.java

--- Playlist.java
+++ Playlist.java
@@ -10,19 +10,22 @@ public class Playlist
      * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
      * (CarDealership, Zoo) for structure on how this will look
      */
-    public ArrayList<Song> playlist;
-
+    private ArrayList<Song> playlist;
+    private String title;
+    private String artist;
+    private int durationInSeconds;
+    private boolean liked;
      /**
       * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
       * then use additional methods to add Songs in one-by-one
       */
-    public Playlist()
+    public Playlist(String title, String artist, int durationInSeconds)
     {
         playlist = new ArrayList<>();
+        this.title = title;
+        this.artist = artist;
+        this.durationInSeconds = durationInSeconds;
     }
-
-      
-
       /**
        * Methods-- Remember that you need to be able to complete all of the following:
        * Adding a song
@@ -33,4 +36,36 @@ public class Playlist
        * Determining the total duration of all songs
        * Removing all unliked songs from the playlist (careful with this one!)
        */
+    public String getTitle()
+    {
+        return title;
+    }
+    public String getArtist()
+    {
+        return artist;
+    }
+    public int getDuration()
+    {
+        return durationInSeconds;
+    }
+    public boolean isLiked()
+    {
+        return liked;
+    }
+    public add(Song newSong)
+    {
+        songs.add(newSong);
+    }
+    public add(String title, String author, int durationInSeconds)
+    {
+        
+    }
+    public int totalDuration()
+    {
+        
+    }
+    public String removeUnliked()
+    {
+        
+    }
 }
