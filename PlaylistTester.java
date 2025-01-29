2358a796076ce79c5f8f97ba5e8ee3451ca8c149
Olivia Ip <oip25@students.polytechnic.org>
2025-01-28 22:10:24 -0800

    Edited song.java and did playlistTester.java

---
+++ PlaylistTester.java
@@ -0,0 +1,78 @@
+public class PlaylistTester {
+    public static void main(String[] args) {
+        System.out.println("Initializing a Playlist...\n");
+        Playlist p = new Playlist();
+        System.out.println("Adding songs to the Playlist...\n");
+
+        System.out.println("Added \"%s\" by %s ( %s)".formatted("This Must Be the Place", "Talking Heads", "4:56)"));
+        p.addSong("Dominoes", "Donald Byrd ", "4:33", true);
+        p.addSong("Check the Rhime", "A Tribe Called Quest", "3:36", false);
+        p.addSong("Con Altura", "TRosalia", "(2:41)", true);
+        p.addSong("California", "Joni Mitchell", "3:50", false);
+        p.addSong("In A Good Way", "Faye Webster", "3:42", false);
+
+        System.out.println("Printing the songs...\n");
+
+        for (int i = 0; i < p.vibes.size(); i++)
+        {
+            System.out.println("%s by %s (%s)".formatted(p.vibes.get(i).getName(), p.vibes.get(i).getArtist(), p.vibes.get(i).songDur()));
+        }
+
+        System.out.println("\nLiking the songs in position 1, 3, 4\n");
+        p.like(p.vibes.get(0).getName());
+        p.like(p.vibes.get(2).getName());
+        p.like(p.vibes.get(3).getName());
+
+        System.out.println("Printing the songs...\n");
+
+        for (int i = 0; i < p.vibes.size(); i++)
+        {
+            if (p.vibes.get(i).isLiked())
+            {
+                System.out.println("%s by %s (%s) --liked".formatted(p.vibes.get(i).getName(), p.vibes.get(i).getArtist(), p.vibes.get(i).songDur()) );
+            }
+            else 
+            {
+
+                System.out.println("%s by %s (%s)".formatted(p.vibes.get(i).getName(), p.vibes.get(i).getArtist(), p.vibes.get(i).songDur()));
+            }
+        }
+
+        System.out.println("\nRemoving the song in position 1 and 3\n");
+
+        p.removeSong(p.vibes.get(1).getName());
+        p.removeSong(p.vibes.get(3).getName());
+
+        System.out.println("Printing the songs...\n");
+
+        for (int i = 0; i < p.vibes.size(); i++)
+        {
+            if (p.vibes.get(i).isLiked())
+            {
+                System.out.println("%s by %s (%s) --liked".formatted(p.vibes.get(i).getName(), p.vibes.get(i).getArtist(), p.vibes.get(i).songDur()) );
+            }
+            else 
+            {
+
+                System.out.println("%s by %s (%s)".formatted(p.vibes.get(i).getName(), p.vibes.get(i).getArtist(), p.vibes.get(i).songDur()));
+            }
+        }
+        System.out.println("\nPrinting only the liked songs...\n");
+
+        for (int i = 0; i < p.vibes.size(); i++)
+        {
+            if (p.vibes.get(i).isLiked())
+            {
+                System.out.println("%s by %s (%s) --liked".formatted(p.vibes.get(i).getName(), p.vibes.get(i).getArtist(), p.vibes.get(i).songDur()) );
+            }
+        }
+        System.out.println("\nPrinting the total duration of all songs...\n");
+
+        int parts = (int) (p.totalDuration());
+        System.out.println(parts+ ":"+ (int) ((p.totalDuration() - parts) * 60));
+
+        System.out.println("\nRemoving all unliked songs from the playlist...\n");
+        System.out.println("Printing all songs...\n");
+
+    }
+}
--- Song.java
+++ Song.java
@@ -4,49 +4,64 @@
  * @version
  */
 public class Song {
-    private String songName;
-    private String songDur;
+    private String name;
+    private String songDuration;
     private boolean liked;
     private String artist;
 
-    public Song(String songName, String artist, String songDur, boolean liked)
+    public Song(String Name, String artist, String songDuration, boolean liked)
     {
-        this.songName = songName;
-        this.songDur = songDur;
+        this.name = name;
+        this.songDuration = songDuration;
         this.liked = false;
         this.artist = artist;
     }
+
     public String getName()
     {
-          return songName;  
+        return name;  
     }
+
     public String getArtist()
     {
         return artist;
     }
-    public String songDur()
+
+    public String songDuration()
     {
-        return songDur;
+        return songDuration;
     }
+
     public boolean isLiked()
     {
         return liked;
     }
+
     public void like()
     {
         liked = true;
     }
+
     public void unlike()
     {
         liked = false;
     }
+
     public double calcDuration()
     {
         double sec = 0;
-        String min = songDur.substring(0, songDur.indexOf(":"));
+        int colonPosition = -1;
+        for (int i = 0; i < songDuration.length(); i++) {
+            if (songDuration.charAt(i) == ':') {
+                colonPosition = i;
+                break;
+            }
+        }
+        String min = songDuration.substring(0, colonPosition);
         sec += Integer.parseInt(min) * 60;
-        String secPart = songDur.substring(songDur.indexOf(":") + 1, songDur.length());
+        String secPart = songDuration.substring(colonPosition + 1);
         sec += Integer.parseInt(secPart);
         return sec;
     }
+
 }
